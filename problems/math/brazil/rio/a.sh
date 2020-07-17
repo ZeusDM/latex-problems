98ebafbb6fd06850f20a851a65a6590ab59d743c
ZeusDM <zeusdanmou@gmail.com>
2020-07-17 14:58:19 -0300

    Adding OMERJ 2019.

--- problems/math/brazil/rio/a.sh
+++ problems/math/brazil/rio/a.sh
@@ -1,15 +1,17 @@
-for i in {1998..2018};
+for i in {2019..2019};
 do
     mkdir $i
-    rm $i/source_pt.tex
-    rm $i/source_en.tex
-    rm $i/source.tex
-	rm $i/N3/source_pt.tex
-	rm $i/N4/source_pt.tex
-	rm $i/NU/source_pt.tex
-	rm $i/N3/source_en.tex
-	rm $i/N4/source_en.tex
-	rm $i/NU/source_en.tex
+	mkdir $i/N3
+	mkdir $i/N4
+    #rm $i/source_pt.tex
+    #rm $i/source_en.tex
+    #rm $i/source.tex
+	#rm $i/N3/source_pt.tex
+	#rm $i/N4/source_pt.tex
+	#rm $i/NU/source_pt.tex
+	#rm $i/N3/source_en.tex
+	#rm $i/N4/source_en.tex
+	#rm $i/NU/source_en.tex
 	echo "OMERJ $i, N3" >> $i/N3/source_pt.tex
     echo "OMERJ $i, N4" >> $i/N4/source_pt.tex
     echo "OMERJ $i, NU" >> $i/NU/source_pt.tex   
@@ -18,6 +20,8 @@ do
 	echo "Rio $i, NU" >> $i/NU/source_en.tex	
 	for j in {1..6}
 	do
+		mkdir $i/N3/$j
+		mkdir $i/N4/$j
 		rm $i/N3/$j/number.tex
 		rm $i/N4/$j/number.tex
 		rm $i/NU/$j/number.tex
